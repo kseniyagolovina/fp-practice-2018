49c7e98997d3591fb9bcbafd47e0d9f735ab76c9
Xeniys <kseniyagolovina@mail.ru>
2018-10-24 02:36:08 +0300

    task 1

--- Task1_1.hs
+++ Task1_1.hs
@@ -1,27 +1,58 @@
-module Task1_1 where
+﻿module Task1_1 where
 
 import Todo(todo)
 
+data Operation = Plus | Minus | Multiply
+            deriving(Show,Eq)
+
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, rhv :: Term, oper :: Operation } -- бинарная операция
             deriving(Show,Eq)
+
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
-(|+|) l r = todo
+(|+|) l r = BinaryTerm l r Plus
 (|-|) :: Term -> Term -> Term
-(|-|) l r = todo
+(|-|) l r = BinaryTerm l r Minus
 (|*|) :: Term -> Term -> Term
-(|*|) l r = todo
+(|*|) l r = BinaryTerm l r Multiply
+
+infixl 5 |+|
+infixl 5 |-|
+infixl 6 |*|
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
-
+replaceVar varName replacement expression = case (expression) of
+          Variable var | var == varName -> replacement
+          BinaryTerm lhv rhv oper -> BinaryTerm (replaceVar varName replacement lhv) (replaceVar varName replacement rhv) oper
+          _ -> expression
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
 evaluate :: Term -> Term
-evaluate expression = todo
+evaluate (BinaryTerm l r oper) = 
+    let lhv = evaluate l in
+    let rhv = evaluate r in
+    case oper of
+    Plus -> case (lhv, rhv) of
+        (IntConstant lhv, IntConstant rhv) -> IntConstant (lhv + rhv)
+        (IntConstant 0, rhv) -> rhv
+        (lhv, IntConstant 0) -> lhv
+        otherwise -> BinaryTerm lhv rhv Plus
+    Minus -> case (lhv, rhv) of
+        (IntConstant lhv, IntConstant rhv) -> IntConstant (lhv - rhv)
+        (lhv, IntConstant 0) -> lhv
+        otherwise -> BinaryTerm lhv rhv Minus
+    Multiply -> case (lhv, rhv) of
+        (IntConstant lhv, IntConstant rhv) -> IntConstant (lhv * rhv)
+        (IntConstant 0, rhv) -> IntConstant 0
+        (IntConstant 1, rhv) -> rhv
+        (lhv, IntConstant 0) -> IntConstant 0
+        (lhv, IntConstant 1) -> lhv
+        otherwise -> BinaryTerm lhv rhv Multiply
+    otherwise -> BinaryTerm l r oper
+evaluate expression = expression
--- Task1_2.hs
+++ Task1_2.hs
@@ -2,6 +2,8 @@ module Task1_2 where
 
 import Todo(todo)
 
+import Prelude hiding (gcd)
+
 -- синус числа (формула Тейлора)
 sin :: Double -> Double
 sin x = todo
@@ -12,25 +14,42 @@ cos x = todo
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y=  
+    if x == 0 then y else
+        if y == 0 then x else
+            if (x < 0) || (y < 0) then gcd (abs x) (abs y) else
+                if x > y then gcd (x - y) y
+                            else gcd (y - x) x
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
-doesSquareBetweenExist from to = todo
-
+doesSquareBetweenExist from to = ceiling (sqrt (fromIntegral from)) ^ 2 < to
+        
 -- является ли дата корректной с учётом количества дней в месяце и
 -- вискокосных годов?
 isDateCorrect :: Integer -> Integer -> Integer -> Bool
-isDateCorrect day month year = todo
+isDateCorrect day month year = 
+    if day <= 0 || month <= 0 || month >= 13 || year <= 0 then False else
+        if month == 2 then day < 29 || (((year `rem` 4 == 0 && year `rem` 100 /= 0) || (year `rem` 400 == 0)) && day == 29) else 
+            if month `elem` [4, 6, 9, 11] then day < 31 
+                else day < 32
+
 
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y =
+    if y < 0 then error ("Denominator less than zero") else
+        if y == 0 then 1 else 
+            if y == 1 then x else
+                if y `mod` 2 == 1 then x * (pow x (y - 1))
+                    else ((pow x (y `div` 2)) * (pow x (y `div` 2)))
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+isPrime x = foldr (\p r-> p*p>x || (rem x p /= 0 && r)) True primeNums
+primeNums = 2 : [x | x <- [3..], isPrime x]
+
 
 type Point2D = (Double, Double)
 
